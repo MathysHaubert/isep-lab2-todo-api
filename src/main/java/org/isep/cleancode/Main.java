8b415275327f9444c4a0831545ccdfc76b8b1244
Mathys Haubert <mathys.haubert80@gmail.com>
2025-05-07 12:12:46 +0200

    bonus config.properties file and testEndpoint (#2)

--- src/main/java/org/isep/cleancode/Main.java
+++ src/main/java/org/isep/cleancode/Main.java
@@ -1,17 +1,39 @@
 package org.isep.cleancode;
 
-import static spark.Spark.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 import org.isep.cleancode.persistence.ITodoRepository;
 import org.isep.cleancode.persistence.csvfiles.TodoCsvFilesRepository;
-import org.isep.cleancode.presentation.TodoController;
+import org.isep.cleancode.persistence.inmemory.TodoInMemoryRepository;
 import org.isep.cleancode.application.TodoManager;
+import org.isep.cleancode.presentation.TodoController;
+
+import static spark.Spark.*;
 
 public class Main {
 
     public static void startApp() {
+        String configFilePath = Paths.get(System.getProperty("user.dir"),"config.properties").toString();
 
-        ITodoRepository repo = new TodoCsvFilesRepository();
+        Properties config = new Properties();
+        try (FileInputStream input = new FileInputStream(configFilePath)) {
+            config.load(input);
+        } catch (IOException e) {
+            System.err.println("Failed to load config file: " + e.getMessage());
+            return;
+        }
+
+        String repoType = config.getProperty("repository", "CSV").toUpperCase();
+
+        ITodoRepository repo = switch (repoType) {
+            case "INMEMORY" -> new TodoInMemoryRepository();
+            case "CSV" -> new TodoCsvFilesRepository();
+            default -> throw new IllegalArgumentException("Unknown repository type in config: " + repoType);
+        };
+
         TodoManager manager = new TodoManager(repo);
         TodoController controller = new TodoController(manager);
 
@@ -28,5 +50,3 @@ public class Main {
         startApp();
     }
 }
-
-
